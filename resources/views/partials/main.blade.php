e8d529b6764a7e4ac8d5ba382b40fc284059aa18
ValleLuca <valle.lucaAb@outlook.it>
2022-03-07 10:18:58 +0100

    Modifiche di codice per il collegamento css

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -3,7 +3,7 @@
         <div class="row row-cols-5">
             @foreach ($finale as $item=>$element)
                 <div class="col text-center">
-                    <div class="border">
+                    <div class="border border-secondary">
                         <p>{{$element->title}}</p>
                         <p>{{$element->nationality}}</p>
                         <p>{{$element->vote}}</p>
