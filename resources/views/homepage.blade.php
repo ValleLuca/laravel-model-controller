14a432b618dd1dcc64047e2c623324747c5158c1
ValleLuca <valle.lucaAb@outlook.it>
2022-03-07 10:13:32 +0100

    modifiche estetiche

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laravelEXtreme</title>
-</head>
-<body>
-    @foreach ($finale as $item=>$element)
-        <div>{{$element->title}}</div> 
-    @endforeach
-    @dump($finale);
-
-</body>
-</html>
+@extends('layout.app')
